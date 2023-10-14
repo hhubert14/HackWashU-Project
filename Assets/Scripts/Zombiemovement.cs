8cea434151bb50a2525a2b7d7a4fbc7df5be1d9f
dabouti <d.h.kim@wustl.edu>
2023-10-14 16:10:13 -0500

    ZombieFail

--- Assets/Scripts/Zombiemovement.cs
+++ Assets/Scripts/Zombiemovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ZombControls : MonoBehaviour
+public class Zombiemovement : MonoBehaviour
 {   
     public float speed = 2.0f; 
     private Transform target;
